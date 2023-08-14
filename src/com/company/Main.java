de7ba491bd4e2da6e409a23dae2771436b0b7ea4
Daniil Danilyuk <dan201103@gmail.com>
2023-08-14 12:16:38 +0300

    Revert "Four commit"
    This reverts commit 6c846f7eca7b221c3b041306d38fee484250bb92.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,10 +6,8 @@ public class Main {
         arrayStorage.save(new Resume(1,"Dan"));
         arrayStorage.save(new Resume(2,"Bob"));
         arrayStorage.save(new Resume(3,"Bolen"));
-        arrayStorage.save(new Resume(3,"Bob"));
 
         System.out.println(arrayStorage.getName("Bob"));
-        System.out.println("Bob");
 
     }   
 
